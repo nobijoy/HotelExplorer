a32ff8ed93346dacfbc10b2d746d85d67b9b6ac1
nobijoy <nobijoy1446@gmail.com>
2023-06-12 01:32:03 +0600

    working on authentication

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,29 @@
 import express from "express";
+import { getUsers, getUser, updateUser, deleteUser } from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.send("this is auth");  
+
+router.get("/checkauthentication", verifyToken, (req, res, next)=> {
+    res.send("hello user you are logged in");
 });
+
+router.get("/checkuser/:id", verifyUser, (req, res, next)=> {
+    res.send("hello user you are logged in and you can delete this account");
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next)=> {
+    res.send("hello admin you are logged in and you can delete this account");
+});
+
+router.get('/', getUsers);
+
+router.get('/:id', getUser);
+
+router.put('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
 
 
 export default router;
